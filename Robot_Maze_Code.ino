dcf98ee1664d8b3a3685a8789764ce899b488830
Yanzhou Wang <yxw723@case.edu>
2018-10-26 19:10:14 -0400

    changed Record() function

--- Robot_Maze_Code.ino
+++ Robot_Maze_Code.ino
@@ -2,10 +2,19 @@
 #include <XBee.h>
 
 File myFile;
+const int chipSelect = 10;
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
-Rx16Response rx16 = Rx16Response();                                         
+Rx16Response rx16 = Rx16Response();
 uint8_t dir{};                              //Direction data
+
+
+int motor1F = 3;
+int motor1B = 5;
+int motor2F = 6;
+int motor2B = 9;
+double speed = 255;
+double turnTime = 1000; //motor turning time; subject to wheel specifications
 
 
 void setup() {
@@ -13,9 +22,9 @@ void setup() {
 
   //SD Card Setup Begin//
   Serial.print("Initializing SD card...");
-  pinMode(10, OUTPUT);
+  pinMode(chipSelect, OUTPUT);
 
-  if (!SD.begin(10)) {
+  if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed!");
     return;
   }
@@ -30,38 +39,88 @@ void setup() {
   xbee.setSerial(Serial);
   //XBee Setup End//
 
+  //Motor Setup Begin//
+  pinMode(motor1F, OUTPUT);
+  pinMode(motor1B, OUTPUT);
+  pinMode(motor2F, OUTPUT);
+  pinMode(motor2B, OUTPUT);
+  //Motor Setup End//
+
   Serial.println("initialization done.");
   Serial.println("Ready!");
 }
 
+void Forward() {
+  analogWrite(motor1F, speed);
+  analogWrite(motor2F, speed);
+  delay(turnTime);
+  analogWrite(motor1F, LOW);
+  analogWrite(motor2F, LOW);
+  delay(500);
+}
+
+void Backward() {
+  analogWrite(motor1B, speed);
+  analogWrite(motor2B, speed);
+  delay(turnTime);
+  analogWrite(motor1B, LOW);
+  analogWrite(motor2B, LOW);
+  delay(500);
+}
+
+void Left() {
+  analogWrite(motor1F, speed);
+  analogWrite(motor2B, speed);
+  delay(turnTime);
+  analogWrite(motor1F, LOW);
+  analogWrite(motor2B, LOW);
+  delay(500);
+}
+
+void Right() {
+  analogWrite(motor1B, speed);
+  analogWrite(motor2F, speed);
+  delay(turnTime);
+  analogWrite(motor1B, LOW);
+  analogWrite(motor2F, LOW);
+  delay(500);
+}
+
 void Record() {
-  xbee.readPacket(1000);
-  // reply only when you receive data:
-  if (xbee.getResponse().isAvailable() &&  xbee.getResponse().getApiId() == RX_16_RESPONSE) {
-    xbee.getResponse().getRx16Response(rx16);
-    dir = rx16.getData(0);
+  //only runs when the file is open and ready to write
+  while (myFile) {
+    xbee.readPacket(1000); //wait 1s for response
+    // reply only when you receive data:
+    if (xbee.getResponse().isAvailable() &&  xbee.getResponse().getApiId() == RX_16_RESPONSE) {
 
-    if (dir == 1) {
-      Serial.println(dir);
-      myFile.write("1");
-    }
-    else if (dir == 3) {
-      Serial.println(dir);
-      myFile.write("3");
+      xbee.getResponse().getRx16Response(rx16);
+      dir = rx16.getData(0);
+      if (dir == 1) {
+        Serial.println(dir);
+        Forward();
+        myFile.write("1");
+      }
+      else if (dir == 3) {
+        Serial.println(dir);
+        Left();
+        myFile.write("3");
 
-    }
-    else if (dir == 5) {
-      Serial.println(dir);
-      myFile.write("5");
+      }
+      else if (dir == 5) {
+        Serial.println(dir);
+        Right();
+        myFile.write("5");
 
-    }
-    else if (dir == 7) {
-      Serial.println(dir);
-      myFile.write("7");
-    }
-    else if (dir == 4) {
-      myFile.close();
-      Serial.println("DONE!");
+      }
+      else if (dir == 7) {
+        Serial.println(dir);
+        Backward();
+        myFile.write("7");
+      }
+      else if (dir == 4) {
+        myFile.close(); //close file after it's done; avoid re-entering Record function.
+        Serial.println("DONE RECORDING!");
+      }
     }
   }
 }
@@ -70,7 +129,7 @@ void Record() {
 
 
 void loop() {
-
   Record();
+  Serial.println("Oops");
 
 }
