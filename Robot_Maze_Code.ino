6748b4751be5bd0d3eb99c07e93fe1af0a3f4ca4
Yanzhou Wang <yxw723@case.edu>
2018-10-26 17:26:10 -0400

    Added Xbee module code

--- Robot_Maze_Code.ino
+++ Robot_Maze_Code.ino
@@ -1,8 +1,17 @@
 #include <SD.h>
-int dir;
+#include <XBee.h>
+
 File myFile;
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx16Response rx16 = Rx16Response();                                         
+uint8_t dir{};                              //Direction data
+
+
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+  //SD Card Setup Begin//
   Serial.print("Initializing SD card...");
   pinMode(10, OUTPUT);
 
@@ -14,38 +23,43 @@ void setup() {
     SD.remove("a.txt");
   }
   myFile = SD.open("a.txt", FILE_WRITE);
+  //SD Card Setup End//
 
 
-  
+  //XBee Setup Begin//
+  xbee.setSerial(Serial);
+  //XBee Setup End//
+
   Serial.println("initialization done.");
   Serial.println("Ready!");
 }
 
 void Record() {
-  Serial.println("In Record:")
+  xbee.readPacket(1000);
   // reply only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    dir = Serial.parseInt();
+  if (xbee.getResponse().isAvailable() &&  xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+    xbee.getResponse().getRx16Response(rx16);
+    dir = rx16.getData(0);
+
     if (dir == 1) {
       Serial.println(dir);
       myFile.write("1");
-    }
-    else if (dir == 2) {
-      Serial.println(dir);
-      myFile.write("2");
-
     }
     else if (dir == 3) {
       Serial.println(dir);
       myFile.write("3");
 
     }
-    else if (dir == 4) {
+    else if (dir == 5) {
       Serial.println(dir);
-      myFile.write("4");
+      myFile.write("5");
+
     }
-    else if (dir == 10) {
+    else if (dir == 7) {
+      Serial.println(dir);
+      myFile.write("7");
+    }
+    else if (dir == 4) {
       myFile.close();
       Serial.println("DONE!");
     }
